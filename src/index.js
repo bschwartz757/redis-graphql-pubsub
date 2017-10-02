05ce1075b67fa29a4767f8759a97de40b0c5fc92
Schwartz, Blake <Blake.H.Schwartz.-ND@disney.com>
2017-10-01 18:24:18 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+import {SubscriptionClient, addGraphQLSubscriptions} from 'subscriptions-transport-ws'
+import App from './components/App'
+// eslint-disable-next-line
+import registerServiceWorker from './registerServiceWorker'
+import './styles/index.css'
+import {GC_AUTH_TOKEN} from './constants'
+// 1
+import {ApolloProvider, createNetworkInterface, ApolloClient} from 'react-apollo'
+
+// 2
+const networkInterface = createNetworkInterface({uri: 'https://api.graph.cool/simple/v1/cj6duaasd3jwe0101a55zyvwe'})
+
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj6duaasd3jwe0101a55zyvwe', {
+  reconnect: true,
+  connectionParams: {
+    authToken: localStorage.getItem(GC_AUTH_TOKEN)
+  }
+})
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(networkInterface, wsClient)
+
+networkInterface.use([
+  {
+    applyMiddleware(req, next) {
+      if (!req.options.headers) {
+        req.options.headers = {}
+      }
+      const token = localStorage.getItem(GC_AUTH_TOKEN)
+      req.options.headers.authorization = token
+        ? `Bearer ${token}`
+        : null
+      next()
+    }
+  }
+])
+
+const client = new ApolloClient({networkInterface: networkInterfaceWithSubscriptions})
+
+// 4
+ReactDOM.render(
+  <BrowserRouter>
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
+</BrowserRouter>, document.getElementById('root'))
